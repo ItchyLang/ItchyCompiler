2a8283db405e3a172d0ca5a55d894430735c0fba
Matty Muir <mattygmuir@gmail.com>
2023-11-05 11:12:54 +0000

    Cleaned up main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,6 @@
-import net.itchy.compiler.lexer.Lexer
-import net.itchy.compiler.parser.Parser
-import net.itchy.scratch.ScratchGenerator
-import net.itchy.scratch.bundling.bundle
+import net.itchy.utils.compileAndBundleFile
 
 fun main()
 {
-    val src = """
-sprite X
-{
-	when init
-	{
-		load_costume("abby-a", "./abby-a.svg")
-		load_backdrop("backdrop", "./backdrop.svg")
-		
-		go_to(x_position + 200, y_position + 200)
-	}
-}
-        """.trimIndent()
-
-    // Lexing
-    val tokens = Lexer(src).lex()
-
-    // Parsing
-    val ast = Parser(tokens).parse()
-
-    // Generating
-    val project = ScratchGenerator().generate(ast)
-
-    // Serialization and Bundling
-    bundle("SaySum", project, ".")
+    compileAndBundleFile("C:\\Users\\matty\\Desktop\\ItchyProj\\main.ich")
 }
